5e534e80ca97d89054445a7f6ea890692092083f
Fahim <fahimurrahman546@gmail.com>
2026-07-14 06:44:54 +0600

    Add ternary practice file

---
+++ module_1/ternarrypr.py
@@ -0,0 +1,14 @@
+# even odd ternarry practice
+# number=int(input("number:"))
+
+# result ="even"if number % 2==0 else "odd"
+
+# print(result)
+
+# temperture terrnary practice
+# temperature=int(input("temperature:"))
+# result="hot"if temperature >=30 else "Cold"  
+# print(result)
+marks=int(input("marks:"))
+result="Excellent"if marks>=80 else ("pass" if marks>=50 else "Fail")
+print(result)
